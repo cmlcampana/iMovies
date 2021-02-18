1adf3c16c8ebd82a5697032e478937951e44c011
Camila Campana <camila.campana@picpay.com>
2021-02-18 16:21:34 -0300

    Adding project

---
+++ iMovie/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iMovie
+//
+//  Created by Camila Campana on 18/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// Pods
+// Snapkit
+// HttpStub
+//
+// Criar alguns padrões de cores/espaçamentos
+
+// API - https://trakt.docs.apiary.io/#
+// gitignore
